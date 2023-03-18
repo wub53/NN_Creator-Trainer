05e00d66fcd0d5a048f4fd8bbd705805883c7d80
Omkar <omkarpatil333333@gmail.com>
2023-03-18 06:29:21 -0400

    My first library

---
+++ NN_Creater.py
@@ -0,0 +1,44 @@
+import random
+
+from NN_engine import Variable
+
+class Neuron:
+  
+  def __init__(self, connections):
+    self.w = [Variable(random.uniform(-1,1)) for iter in range(connections)]
+    self.b = Variable(random.uniform(-1,1))
+  
+  def __call__(self, x):
+    # w * x + b
+    act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
+    squished_act = act.tanh()
+    return squished_act
+  
+  def parameters(self):
+    return self.w + [self.b]
+
+class Layer:
+  
+  def __init__(self, connections, num_neurons):
+    self.neurons = [Neuron(connections) for iter in range(num_neurons)]
+  
+  def __call__(self, x):
+    outs = [n(x) for n in self.neurons]
+    return outs[0] if len(outs) == 1 else outs
+  
+  def parameters(self):
+    return [p for neuron in self.neurons for p in neuron.parameters()]   ##
+
+class MLP:
+  
+  def __init__(self, connections, layers_exluding_ip):
+    mlp_skeleton = [connections] + layers_exluding_ip
+    self.layers = [Layer(mlp_skeleton[i], mlp_skeleton[i+1]) for i in range(len(layers_exluding_ip))]
+  
+  def __call__(self, x):
+    for layer in self.layers:
+      x = layer(x)
+    return x
+  
+  def parameters(self):
+    return [p for layer in self.layers for p in layer.parameters()]
---
+++ training.py
@@ -0,0 +1,56 @@
+import NN_Creater as nn
+
+def flush_gradients(mlp):
+  for i in mlp.parameters():
+      i.grad = 0
+
+def nudge_the_weightsbiases(mlp):
+  for i in mlp.parameters():
+      i.value += -0.1 * i.grad
+
+def forward_pass(mlp,inputs):
+  ypred = [mlp(x) for x in inputs]
+  return ypred
+
+def back_prop(mlp,loss):
+  flush_gradients(mlp)
+  loss.backward() 
+
+def calc_loss(ydesired, ypred):
+  # L is the list of loss for each input example
+  L = [(yout - ygt)**2 for ygt,yout in zip(ydesired, ypred)]
+  #Summing the loss for every 
+  loss = nn.Variable(0)
+  for i in L:
+    loss = loss + i
+  return loss
+  
+
+# NN or (Multi level perceptron if you may) will be of 3,4,4,1 neurons per layer(4) last one being the output layer
+my_nn = nn.MLP(3, [4, 4, 1])
+
+#xs is the list of inputs we are going to feed the my_nn 
+xs = [
+  [2.0, 3.0, -1.0],
+  [3.0, -1.0, 0.5],
+  [0.5, 1.0, 1.0],
+  [1.0, 1.0, -1.0],
+]
+ydesired = [1.0, -1.0, -1.0, 1.0] # desired targets
+
+ypred=[]
+
+## Every iteraton of this loop is an epoch !!
+for i in range(1,15):
+  ypred = forward_pass(my_nn,xs)
+
+  loss = calc_loss(ydesired,ypred)
+  print(loss)
+
+  back_prop(my_nn,loss)
+
+  nudge_the_weightsbiases(my_nn)
+
+
+
+
